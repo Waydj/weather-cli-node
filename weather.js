e95cd33cfe56e5953a8d9a8cc63d6b0092f4c2e0
Waydj <farhminait@gmail.com>
2022-07-23 17:26:03 +0200

    Начало проекта, подготовка аргументов

---
+++ weather.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+import {getArgs} from "./helpers/args.js";
+
+const initCLI = () => {
+  const args = getArgs(process.argv)
+  console.log(args)
+  if(args.h) {
+    //  help
+  }
+  if(args.s) {
+    //  set city
+  }
+  if(args.t) {
+    //  set token
+  }
+//  weather
+}
+
+initCLI()
